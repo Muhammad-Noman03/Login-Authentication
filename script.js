89273adf33d858ff526f1b0c54c11163a0a292b9
Muhammad-Noman03 <nomireigns@gmail.com>
2024-06-04 20:10:26 +0500

    added email and password validation with regex

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const signUpPassword = document.querySelector('#signUpPassword');
 const confirmPassword = document.querySelector('#confirmPassword');
 const signUpBtn = document.querySelector('#signUpBtn');
 
+// VARIABLES
+const data = []
+const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
 
 signUpInputs.forEach(input => {
@@ -13,11 +16,34 @@ signUpInputs.forEach(input => {
 })
 
 
+signUpBtn.addEventListener('click', () => {
+    const getValues = getData(name, signUpEmail, signUpPassword, confirmPassword);
 
+    const valid = checkValidation(getValues);
+    console.log(valid);
 
+    if (valid) {
+        data.push(getValues);
+    }
 
+})
 
+// FUNCTION TO GET DATA FROM SIGN UP INPUTS
+function getData(n, se, sp, cp) {
+    const value = {
+        name: n.value,
+        email: se.value,
+        password: sp.value,
+        confirmPassword: cp.value
+    }
 
+    n.value = ''
+    se.value = ''
+    sp.value = ''
+    cp.value = ''
+
+    return value
+}
 
 // FUNCTION TO CHECK EMPTY SIGN UP INPUTS
 function checkInput(input, btn) {
@@ -33,4 +59,20 @@ function checkInput(input, btn) {
         btn.classList.remove('not-filled')
     }
     console.log(btn.disabled);
+}
+
+// FUNCTION TO CHECK EMAIL VALIDATION AND PASSWORD CONFIRMATION
+function checkValidation(data) {
+
+    const { email, password, confirmPassword } = data;
+    console.log(email);
+    console.log(password);
+    console.log(confirmPassword);
+    console.log(emailRegEx.test(email));
+
+    if (!emailRegEx.test(email) || password !== confirmPassword) {
+        return false;
+    }
+
+    return true;
 }
